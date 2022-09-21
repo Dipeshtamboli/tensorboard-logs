90cb2bb3a71580a86518205bfa1ae78f82af3146
Dipesh Tamboli <dipeshtamboli@Dipeshs-MacBook-Pro.local>
2022-09-21 09:00:39 -0400

    start remote

---
+++ only_funcs.py
@@ -0,0 +1,35 @@
+from torch.utils.tensorboard import SummaryWriter
+import numpy as np
+
+writer = SummaryWriter(log_dir= "only_funcs" )
+
+for n_iter in range(100):
+    writer.add_scalar('Loss/train', np.random.random(), n_iter)
+    writer.add_scalar('Loss/test', np.random.random(), n_iter)
+    writer.add_scalar('Accuracy/train', np.random.random(), n_iter)
+    writer.add_scalar('Accuracy/test', np.random.random(), n_iter)
+
+r = 5
+for i in range(100):
+    writer.add_scalars('run_14h', {'xsinx':i*np.sin(i/r),
+                                    'xcosx':i*np.cos(i/r),
+                                    'tanx': np.tan(i/r)}, i)
+
+import keyword
+import torch
+meta = []
+while len(meta)<100:
+    meta = meta+keyword.kwlist # get some strings
+meta = meta[:100]
+
+for i, v in enumerate(meta):
+    meta[i] = v+str(i)
+
+label_img = torch.rand(100, 3, 10, 32)
+for i in range(100):
+    label_img[i]*=i/100.0
+
+writer.add_embedding(torch.randn(100, 5), metadata=meta, label_img=label_img)
+writer.add_embedding(torch.randn(100, 5), label_img=label_img)
+writer.add_embedding(torch.randn(100, 5), metadata=meta)                                    
+writer.close()
