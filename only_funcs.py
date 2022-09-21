53323d41e813ce7e411701e7f45362bccd59c5cb
Dipesh Tamboli <dipeshtamboli@Dipeshs-MacBook-Pro.local>
2022-09-21 09:15:29 -0400

    added mesh and hparams

--- only_funcs.py
+++ only_funcs.py
@@ -29,7 +29,41 @@ label_img = torch.rand(100, 3, 10, 32)
 for i in range(100):
     label_img[i]*=i/100.0
 
-writer.add_embedding(torch.randn(100, 5), metadata=meta, label_img=label_img)
-writer.add_embedding(torch.randn(100, 5), label_img=label_img)
-writer.add_embedding(torch.randn(100, 5), metadata=meta)                                    
+for i in range(10):
+    writer.add_embedding(torch.rand(100, 5), metadata=meta, label_img=label_img, global_step=i, tag = "random")
+# writer.add_embedding(torch.randn(100, 5), metadata=meta, label_img=label_img)
+# writer.add_embedding(torch.randn(100, 5), label_img=label_img)
+# writer.add_embedding(torch.randn(100, 5), metadata=meta)                                    
+
+
+from torch.utils.tensorboard import SummaryWriter
+vertices_tensor = torch.as_tensor([
+    [1, 1, 1],
+    [-1, -1, 1],
+    [1, -1, -1],
+    [-1, 1, -1],
+], dtype=torch.float).unsqueeze(0)
+colors_tensor = torch.as_tensor([
+    [255, 0, 0],
+    [0, 255, 0],
+    [0, 0, 255],
+    [255, 0, 255],
+], dtype=torch.int).unsqueeze(0)
+faces_tensor = torch.as_tensor([
+    [0, 2, 3],
+    [0, 3, 1],
+    [0, 1, 2],
+    [1, 3, 2],
+], dtype=torch.int).unsqueeze(0)
+
+writer = SummaryWriter()
+writer.add_mesh('my_mesh', vertices=vertices_tensor, colors=colors_tensor, faces=faces_tensor)
+
+
 writer.close()
+
+from torch.utils.tensorboard import SummaryWriter
+with SummaryWriter() as w:
+    for i in range(5):
+        w.add_hparams({'lr': 0.1*i, 'bsize': i},
+                      {'hparam/accuracy': 10*i, 'hparam/loss': 10*i})
